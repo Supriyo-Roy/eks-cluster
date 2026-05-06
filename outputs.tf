0958aa11534764d4ef107de8309549f774fb6bda
Supriyo Roy <supriyoroy@Supriyos-MacBook-Air.local>
2026-05-06 13:59:02 +0530

    added 1st few files

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "private_subnets"{
-    value = module.vpc.private_subnets
-}
-output "vpc_id"{
-    value = module.vpc.vpc_id
-}
+# output "private_subnets"{
+#     value = module.vpc.private_subnets
+# }
+# output "vpc_id"{
+#     value = module.vpc.vpc_id
+# }
